b520cd407676aeb50fc207d1a4b8e3153091bd20
Doruk <doruk.ulucay@cronom.com>
2018-03-23 14:00:46 +0300

    initial commit.

---
+++ hangm.js
@@ -0,0 +1,58 @@
+var HangM = {};
+
+HangM.dictionary = ["computer", "transistor", "lumberjack", "bandwagon", "bookworm"];
+
+HangM.play = function() {
+
+    var getStatu = function(word, given) {
+
+        var statu = "";
+
+        for (w in word) {
+            var wo = word[w];
+            var now = "_";
+
+            for (g in given) {
+                var gi = given[g];
+
+                if (wo === gi) {
+                    now = given[g];
+                    break;
+                }
+            }
+            statu += now;
+        }
+
+        return statu;
+    }
+
+    var given = [];
+    var lives = 5;
+    var word = this.dictionary[Math.floor(Math.random() * this.dictionary.length)];
+    var statu = getStatu(word, given);
+
+    while (statu.indexOf("_") !== -1 && lives !== 0) {
+        var l = prompt(statu + ". gimme a letter.");
+
+        given.push(l);
+
+        statu = getStatu(word, given);
+
+        if (word.indexOf(l) === -1) {
+            lives--;
+            if (lives === 0) {
+                alert("word was " + word + ". you DED.")
+                break;
+            } else {
+
+                alert("wrong. lifes left: " + lives.toString());
+            }
+        } else {
+
+            if (statu.indexOf("_") === -1) {
+                alert("you lived. happily. everafter.")
+                break;
+            }
+        }
+    }
+}
